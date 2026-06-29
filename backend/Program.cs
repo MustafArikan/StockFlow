37ad8f7787b4ffaf635ef46ae2ef7502d7deed6c
MustafArikan <kankimustafarikan@gmail.com>
2026-06-29 16:53:26 +0300

    feat(auth): create register/login DTOs and configure JWT authentication services

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,12 +1,47 @@
+using System.Text;
 using Scalar.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using stok_takip.Data;
+using stok_takip.DTOs;
+using stok_takip.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+var jwtSecretKey = builder.Configuration["JwtSettings:SecretKey"]
+    ?? "A_VERY_LONG_AND_SECURE_SECRET_KEY_FOR_JWT_SIGNING_32_BYTES_MINIMUM!";
+var jwtIssuer = builder.Configuration["JwtSettings:Issuer"] ?? "StockFlowBackend";
+var jwtAudience = builder.Configuration["JwtSettings:Audience"] ?? "StockFlowFrontend";
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtIssuer,
+        ValidAudience = jwtAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecretKey))
+
+    };
+});
+
 
 var app = builder.Build();
 
@@ -18,6 +53,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
