6012e109cbba4d45fbddfc29f2d450f8a8e679a8
MustafArikan <kankimustafarikan@gmail.com>
2026-07-29 16:16:36 +0300

    fix(backend) : Add migration and update database

---
+++ backend/Migrations/20260729131145_AddLocationID.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocationID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "source_location_id",
+                table: "stock_movements",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "target_location_id",
+                table: "stock_movements",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "source_location_id",
+                table: "stock_movements");
+
+            migrationBuilder.DropColumn(
+                name: "target_location_id",
+                table: "stock_movements");
+        }
+    }
+}
